a74afd299f6248d0e61da1fe0638448a040bb12f
Rume7 <electengrhume@gmail.com>
2025-07-26 03:45:24 +0100

    Remove unused LogIn import from LoginPage.js to fix frontend build

--- Blog-app-fe/src/pages/LoginPage.js
+++ Blog-app-fe/src/pages/LoginPage.js
@@ -1,6 +1,5 @@
 import React, { useState, useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
-import { LogIn } from 'lucide-react';
 
 // Login Page
 const LoginPage = ({ onNavigate }) => {
